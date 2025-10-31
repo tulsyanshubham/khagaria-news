edb3f62991abed35202780a8efdde6c11dddcaf0
Shubham Tulsyan <tulsyanshubham3639@gmail.com>
2025-10-31 22:50:26 +0530

    Update Footer.tsx
    Making footer hidden on all admin pages

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -52,7 +52,7 @@ const Footer = () => {
     ];
 
     return (
-        <footer className={cn("bg-linear-to-br from-slate-900 via-gray-900 to-black text-white",pathname === "/login" ? "hidden" : "")}>
+        <footer className={cn("bg-linear-to-br from-slate-900 via-gray-900 to-black text-white",pathname === "/login" || pathname.startsWith("/admin") ? "hidden" : "")}>
             {/* Main Footer Content */}
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12">
